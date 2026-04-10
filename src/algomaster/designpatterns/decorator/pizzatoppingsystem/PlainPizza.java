c90a60b7cc46e5c091ecea437702dba68b707d19
Vishal Kushwaha <vishal.kushwaha@oracle.com>
2026-04-10 20:50:37 +0530

    Fixed bug in decorator system

---
+++ src/algomaster/designpatterns/decorator/pizzatoppingsystem/PlainPizza.java
@@ -0,0 +1,15 @@
+package algomaster.designpatterns.decorator.pizzatoppingsystem;
+
+public class PlainPizza implements Pizza{
+
+    @Override
+    public double getCost() {
+        return 5.0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Plain pizza.";
+    }
+    
+}
