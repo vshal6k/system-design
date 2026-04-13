1977d14abbc19c33b124d1cb4a9757798bb42688
Vishal Kushwaha <vishal.kushwaha@oracle.com>
2026-04-13 23:05:36 +0530

    Tested Board method in Tic Tac Toe System.

--- src/algomaster/problems/tictactoe/Board.java
+++ src/algomaster/problems/tictactoe/Board.java
@@ -4,23 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-
+    private int totalRows;
+    private int totalCols;
     private List<List<Cell>> grid = new ArrayList<>();
     private int totalCells;
     private int cellsFilled;
 
-    public Board(int row, int col){
-        for(int i = 0; i < row; i++){
+    public Board(int totalRows, int totalCols){
+
+        if(totalCols <= 0 || totalRows <= 0) {
+            throw new IllegalArgumentException("Please provide a valid value for total number of rows and columns.");
+        }
+
+        this.totalRows = totalRows;
+        this.totalCols = totalCols;
+        this.totalCells = totalCols * totalCols;
+        this.cellsFilled = 0;
+
+        for(int i = 0; i < totalRows; i++){
             grid.add(new ArrayList<>());
-            for(int j = 0; j < col; j++){
+            for(int j = 0; j < totalCols; j++){
                 grid.get(i).add(new Cell());
             }
         }
-        this.totalCells = row*col;
-        this.cellsFilled = 0;
+        
+    }
+
+    public boolean validateRow(int row){
+        if(row >= 0 && row < totalRows) return true;
+        else throw new IllegalArgumentException("Please provide a valid value for row.");
+    }
+
+    public boolean validateColumn(int column){
+        if (column >= 0 && column < totalCols) return true;
+        else throw new IllegalArgumentException("Please provide a valid value for column.");
     }
 
     public Symbol getSymbol(int row, int col){
+        validateRow(row);
+        validateColumn(col);
         return this.grid.get(row).get(col).getSymbol();
     }
 
@@ -34,11 +56,24 @@ public class Board {
     }
 
     public void addSymbol(int row, int col, Symbol symbol){
-        
+        validateRow(row);
+        validateColumn(col);
+        this.grid.get(row).set(col, new Cell(symbol));
+        this.cellsFilled++;
     }
 
     public boolean isFull(){
         return totalCells == cellsFilled;
     }
+
+    public static void main(String[] args) {
+        Board myBoard = new Board(5, 5);
+        myBoard.printBoard();
+        System.out.println(myBoard.isFull());
+        myBoard.addSymbol(0, 1, Symbol.O);
+        myBoard.printBoard();
+        System.out.println(myBoard.getSymbol(0, 0));
+    }
+
 
 }
--- src/algomaster/problems/tictactoe/Cell.java
+++ src/algomaster/problems/tictactoe/Cell.java
@@ -3,12 +3,24 @@ package algomaster.problems.tictactoe;
 public class Cell {
     private Symbol symbol = Symbol.EMPTY;
 
+    public Cell(Symbol symbol){
+        this.symbol = symbol;
+    }
+
+    public Cell(){
+        this(Symbol.EMPTY);
+    }
+
     public Symbol getSymbol() {
         return symbol;
     }
 
     public void setSymbol(Symbol symbol) {
         this.symbol = symbol;
+    }
+
+    public boolean isEmpty(){
+        return this.symbol.equals(Symbol.EMPTY);
     }
     
 }
